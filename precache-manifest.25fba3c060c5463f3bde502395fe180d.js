92a993d5b788a20db236f80f74f37be9b14b13c5
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-07-28 20:58:14 -0700

    Updates

---
+++ precache-manifest.25fba3c060c5463f3bde502395fe180d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3fc142fc2fedf8e914b6c4fd3f14d2",
+    "url": "/Employee_Directoy/index.html"
+  },
+  {
+    "revision": "32656250c3eba28a3d98",
+    "url": "/Employee_Directoy/static/css/main.9faf5e4f.chunk.css"
+  },
+  {
+    "revision": "4a4a4b59ccec60f8c8ee",
+    "url": "/Employee_Directoy/static/js/2.7e2895e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Directoy/static/js/2.7e2895e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32656250c3eba28a3d98",
+    "url": "/Employee_Directoy/static/js/main.272a81f9.chunk.js"
+  },
+  {
+    "revision": "160792072f842e9ad237",
+    "url": "/Employee_Directoy/static/js/runtime-main.542d6ade.js"
+  }
+]);
